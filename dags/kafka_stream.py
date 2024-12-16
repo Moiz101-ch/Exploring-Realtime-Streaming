7e6d7dc96c580aa0697621b320ea37d92743d8e7
Moiz101-ch <amoizn9@gmail.com>
2024-12-16 23:02:23 +0100

    Updated kafka_stream.py file to successfully format and get the
    Updated kafka_stream.py file to successfully retrieve and format the data from the API.

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -22,21 +22,21 @@ def format_data(res):
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
-    data['address'] = str(location['street']['number']) + ' ' + location['street']['name']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
 
-
+    return data
 
 def stream_data():
-
-
-# with DAG('user_data_automation',
-#          default_args=default_args,
-#          schedule_interval='@daily',
-#          catchup=False) as dag:
-    
-#     streaming_task = PythonOperator(
-#         task_id = 'stream_data_from_api',
-#         python_callable= stream_data
-#     )
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res, indent=3))
 
 stream_data()
