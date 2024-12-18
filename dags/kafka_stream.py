c99713ef87e7e703f01fe65f0ed3d628f31c0737
Moiz101-ch <amoizn9@gmail.com>
2024-12-19 00:11:38 +0100

    Files correctly adjusted in the project directory
    Project directory updated to correctly adjust the kafka_stream.py, docker-compose.yml, requirements.txt and entrypoint.sh files.

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -41,8 +41,18 @@ def stream_data():
     res = format_data(res)
     print(json.dumps(res, indent=3))
 
-    producer = KafkaProducer(bootstrap_servers = ['localhost:9092'], max_block_ms = 5000)
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms = 5000)
 
     producer.send('users_created', json.dumps(res).encode('utf-8'))
 
 stream_data()
+
+with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
+
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
